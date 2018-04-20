5d3c8a6b2dd71b0c9e6887d748bf4e10f9ae2a82
Teerapat <theerapat.nick@gmail.com>
2018-04-20 21:37:28 +0700

    Fix gravity, add animation and crouching. Add sample audio.
    Sample audio added with spatial demo in character playground scene.
    When crouching, can only stand up if no object above.

--- Assets/Script/CharacterControllerScript.cs
+++ Assets/Script/CharacterControllerScript.cs
@@ -7,24 +7,58 @@ public class CharacterControllerScript : MonoBehaviour {
 	private CharacterController characterController;
 	[SerializeField] private float speed;
 	[SerializeField] private float gravity;
-	private Vector3 moveDirection = Vector3.zero;
+	[SerializeField] private float crouchFactor;
+	[SerializeField] private float fallingSpeed;
+	private Vector3 moveDirection;
+	private float x, z;
+	private Animator anim;
+	[SerializeField] private bool standable;
+
+	private float height;
 
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController>();
+		anim = GetComponent<Animator>();
+		height = characterController.height;
+		standable = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float x = Input.GetAxis("Horizontal");		
-		float z = Input.GetAxis("Vertical");
+		x = Input.GetAxis("Horizontal");		
+		z = Input.GetAxis("Vertical");
+		if (x != 0 || z != 0) {
+			anim.SetBool("isWalk", true);
+		} else {
+			anim.SetBool("isWalk", false);
+		}
+		if (Input.GetAxisRaw("Crouch") > 0) {
+			anim.SetBool("isCrouch", true);
+			characterController.center = new Vector3(characterController.center.x, height*crouchFactor/2, characterController.center.z);
+			characterController.height = height*crouchFactor;
+		} else {
+			if (standable) {
+				anim.SetBool("isCrouch", false);
+				characterController.center = new Vector3(characterController.center.x, height/2, characterController.center.z);
+				characterController.height = height;
+			}
+		}
 		transform.Rotate(new Vector3(0, PlayerSetting.CAMERA_SPEED * Input.GetAxis("Mouse X"), 0));
-		moveDirection = new Vector3(z, characterController.isGrounded ? 0 : moveDirection.y, -x);
-		// if (characterController.isGrounded) 
-		print(characterController.isGrounded);
-		moveDirection = transform.TransformDirection(moveDirection);
-		moveDirection *= speed;
-		moveDirection.y -= gravity * Time.deltaTime;
-		characterController.Move(moveDirection * Time.deltaTime);
+		fallingSpeed += gravity;
+		if (characterController.isGrounded) fallingSpeed = 0;
+		moveDirection = new Vector3(z, 0, -x);
+		moveDirection = transform.TransformDirection(moveDirection) * speed * Time.deltaTime;
+		moveDirection.y -= fallingSpeed * Time.deltaTime;
+		characterController.Move(moveDirection);
 	}
+
+	private void OnTriggerStay(Collider other) {
+		if (other != this) standable = false;
+	}
+	private void OnTriggerExit(Collider other) {
+		standable = true;
+	}
+
+	
 }
