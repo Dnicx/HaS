d0f7048d646a19190ec661480dd8db8f38ba8387
Teerapat <theerapat.nick@gmail.com>
2018-04-22 15:42:41 +0700

    Pulling asset

---
+++ Assets/Script/customNetworkManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class customNetworkManager : NetworkManager {
+
+	[SerializeField] GameObject playerPrefab2;
+
+}
