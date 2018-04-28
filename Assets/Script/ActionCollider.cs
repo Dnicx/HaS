00c41120d3dcedb17da8e0613500df08ca8b58a8
Kongnut <kongnut.s@hotmail.com>
2018-04-28 15:10:48 +0700

    Door

---
+++ Assets/Script/ActionCollider.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionCollider : MonoBehaviour {
+
+    private GameObject interactObj;
+    private bool isAction;
+
+	// Use this for initialization
+	void Start () {
+        isAction = false;
+        interactObj = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(interactObj != null)
+        {
+            InteractabltObj obj = interactObj.GetComponent<InteractabltObj>();
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isAction = true;
+                obj.actionDirection = this.transform.forward.normalized;
+                obj.action();
+            }
+            else if (Input.GetKeyUp(KeyCode.Space))
+            {
+                isAction = false;
+                obj.noAction();
+            }
+        }
+        else
+        {
+            isAction = false;
+        }
+	}
+
+    public bool IsAction()
+    {
+        return isAction;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interact Obj"))
+        {
+            interactObj = other.gameObject;
+            
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interact Obj"))
+        {
+            InteractabltObj obj = interactObj.GetComponent<InteractabltObj>();
+            obj.noAction();
+            interactObj = null;
+        }
+    }
+}
---
+++ Assets/Script/InteractabltObj.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InteractabltObj : MonoBehaviour {
+    protected bool isAction;
+    public Vector3 actionDirection;
+
+    public void action()
+    {
+        isAction = true;
+    }
+
+    public void noAction()
+    {
+        isAction = false;
+    }
+}
